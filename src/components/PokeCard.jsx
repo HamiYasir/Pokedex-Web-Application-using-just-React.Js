e45a4450b520a87b0ece05d6ffa5a3ea16aac4ff
HAMI <hamiyasir06@gmail.com>
2025-02-16 09:34:20 +0530

    commit #4: Now all the pokemon sprites, stats and moves are lsited

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -12,6 +12,18 @@ export default function PokeCard(props){
     // Destructring the fetched data
     const { name, height, abilities, stats, types, moves, sprites } = data || {};
 
+    // imgList will get all the keys from the sprites
+    const img_list = Object.keys(sprites || {}).filter(val =>{
+        // If a sprite value is empty, ie; it does not hold any image
+        if(!sprites[val]){return false}
+        // We will exclude the sprites of of the pokemon from versions and others category. We only want the main sprites
+        // If the val includes 'versions', 'other'
+        if(['versions', 'other'].includes(val)){return false}
+        // If the sprite is not any of the above, return that
+        // Else return true
+        return true
+    });
+
     useEffect(()=>{
         // if loading, exit 
         if(loading || !localStorage || data){return}
@@ -80,6 +92,37 @@ export default function PokeCard(props){
                         <TypeCard key={type_index} type={type_obj?.type?.name}/>
                     )
                 })}
+            </div>
+            <img className="default-img" src={'/pokemon/'+getFullPokedexNumber(selected_pokemon)+'.png'} alt={`${name}-large-img`}/>
+            <div className="img-container">
+                {img_list.map((sprite_url, sprite_index)=>{
+                    // console.log(sprite);
+                    return(
+                        <img key={sprite_index} src={sprites[sprite_url]} alt={`${name}-img-${sprite_url}`}/>
+                    )
+                })}
+            </div>
+            <div className="stats-card">
+                {stats.map((stat_obj, stat_index)=>{
+                    const { stat, base_stat } = stat_obj
+                    return(
+                        <div key={stat_index} className="stat-item">
+                            <p>{stat?.name.replaceAll('-', ' ')}</p>
+                            <h4>{base_stat}</h4>
+                        </div>
+                    )
+                })}
+            </div>
+            <h3>Moves</h3>
+            <div className="pokemon-move-grid">
+            {moves.map((move_obj, move_index)=>{
+                // console.log(move_obj['move'].name);
+                return(
+                    <button key={move_index} className="button-card pokemon-move">
+                        <p>{move_obj?.move?.name.replaceAll('-', ' ')}</p>
+                    </button>
+                )
+            })}
             </div>
         </div>
     )
